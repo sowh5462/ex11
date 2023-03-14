99e70c2b4232e46189c1463048846abaa5bab389
sowh5462 <sowh5462@naver.com>
2023-03-14 09:16:20 +0900

    내용

--- src/App.js
+++ src/App.js
@@ -2,11 +2,11 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
+    return (
+        <div className="App">
+            <h1>리액트시작</h1>
+        </div>
+    );
 }
 
 export default App;
