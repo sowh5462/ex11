e8f9c11e98e1dd17c89ccae8f268b690ae9d6bde
sowh5462 <sowh5462@naver.com>
2023-03-14 09:36:02 +0900

    ver02

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
-import logo from './logo.svg';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Button } from 'react-bootstrap';
 import './App.css';
+
 
 function App() {
     return (
         <div className="App">
             <h1>리액트시작</h1>
+            <Button variant="outline-secondary">버튼</Button>
         </div>
     );
 }
